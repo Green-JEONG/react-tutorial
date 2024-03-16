85b954cbc0010241de5a31052fbe0197f231d4fc
정그린 <th2gr22n@gmail.com>
2024-03-16 20:28:55 +0900

    [#1] hello-world 찍기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (<h1>hello world</h1>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
   );
 }
 
-export default App;
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
